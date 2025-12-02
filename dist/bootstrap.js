cdeefbc2a8a43d9b746112a17466fde6f66a80f7
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-12-02 16:58:21 +0200

    update createOtp file, fix nanoid error

--- dist/bootstrap.js
+++ dist/bootstrap.js
@@ -6,15 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const app = (0, express_1.default)();
+const routes_1 = __importDefault(require("./routes"));
+const Errors_1 = require("./utils/Errors");
+const cors_1 = __importDefault(require("cors"));
+const db_connection_1 = require("./DB/db.connection");
 dotenv_1.default.config({
     path: path_1.default.resolve("./src/config/.env"),
 });
-const routes_1 = __importDefault(require("./routes"));
-const db_connection_1 = require("./DB/db.connection");
-const Errors_1 = require("./utils/Errors");
-const cors_1 = __importDefault(require("cors"));
-// import "./utils/amazon/amazon.node.cron"; // To run amazon.node.cron.ts
+const app = (0, express_1.default)();
+// DB connection should run once when function initializes
+(0, db_connection_1.connectDB)();
 var whitelist = [
     "http://example1.com",
     "http://example2.com",
@@ -31,21 +32,15 @@ var corsOptions = {
         }
     },
 };
-const bootstrap = async () => {
-    await (0, db_connection_1.connectDB)();
-    app.use((0, cors_1.default)(corsOptions));
-    app.use(express_1.default.json());
-    app.use("/api/v1", routes_1.default);
-    app.use((err, req, res, next) => {
-        res.status(err.statusCode || 500).json({
-            errMsg: err.message,
-            status: err.statusCode || 500,
-            stack: err.stack,
-        });
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+app.use("/api/v1", routes_1.default);
+// Global error handler
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        errMsg: err.message,
+        status: err.statusCode || 500,
+        stack: err.stack,
     });
-    const httpServer = app.listen(process.env.PORT, () => {
-        console.log("Backend server is running on port", process.env.PORT);
-        console.log("=========================================");
-    });
-};
-exports.default = bootstrap;
+});
+exports.default = app; // THIS IS IMPORTANT
--- src/utils/createOtp.ts
+++ src/utils/createOtp.ts
@@ -1,6 +1,6 @@
-import { customAlphabet } from "nanoid";
-
-export const createOtp = () => {
+// TypeScript version
+export const createOtp = async () => {
+  const { customAlphabet } = await import("nanoid");
   const otp = customAlphabet("0123456", 6)();
   return otp;
 };
